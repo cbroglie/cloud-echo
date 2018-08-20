386e741081b945a175780ea9de8eae0808a4798c
Christopher Broglie <cbroglie@cloudflare.com>
2018-08-19 17:22:31 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tmpl = template.Must(template.New("output").Parse(`{{ .banner }}
+{{ with .headers }}
+Request headers:
+{{ range $key, $value := . }}
+  {{- $key }}: {{ $value }}
+{{ end }}
+{{ end }}
+`))
+
+const cloud = "" +
+	"        _  _\n" +
+	"       ( `   )_\n" +
+	"      (    )    `)\n" +
+	"    (_   (_ .  _) _)\n"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+
+	banner := os.Getenv("BANNER")
+	if banner == "" {
+		banner = cloud
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := tmpl.Execute(w, map[string]interface{}{
+			"banner":  banner,
+			"headers": r.Header,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	http.ListenAndServe(":"+port, nil)
+}
